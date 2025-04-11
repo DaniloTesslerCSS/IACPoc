910414f22c5b4970986677b7fc7e2430a4918518
DaniloTesslerCSS <127798280+DaniloTesslerCSS@users.noreply.github.com>
2025-04-11 13:09:53 -0700

    .

--- app/Pages/Index.cshtml.cs
+++ app/Pages/Index.cshtml.cs
@@ -12,6 +12,8 @@ public class IndexModel : PageModel
 
     public string Weather { get; set; }
 
+    public string BgColor{ get; set; }
+
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
@@ -20,9 +22,13 @@ public class IndexModel : PageModel
     public void OnGet()
     {
         Environment = System.Environment.GetEnvironmentVariable("environment");
+        BgColor = System.Environment.GetEnvironmentVariable("bgcolor");
         
         if (null == Environment)
             Environment = "NOT PROVIDED";
+
+        if (null == BgColor)
+            BgColor = "FFFF00";
             
         Weather = QueryAPI();
     }
@@ -31,31 +37,40 @@ public class IndexModel : PageModel
     {
         string ret = "";
 
-        HttpClient client = new HttpClient();
-
-        client.BaseAddress = new Uri("https://api.open-meteo.com");
-        client.DefaultRequestHeaders.Accept.Clear();
-        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-        
-        Random rnd = new();
-
-        string path = "/v1/forecast?latitude=49.1011&longitude=-122.6588&current=temperature_2m&daily=temperature_2m_max,temperature_2m_min&forecast_days=1";
-
         try
         {
-            HttpResponseMessage response = client.GetAsync(path).Result;
-            if (response.IsSuccessStatusCode)
-            {
-                ret = response.Content.ReadAsStringAsync().Result;
-            }
-            else
-                ret = "Calling the service got " + response.StatusCode + "\r\n" + response.Content;
-        }
-        catch (Exception ex)
-        {
-            ret = "Erro calling " + client.BaseAddress + "\r\n" + ex.ToString();
-        }
 
+            HttpClient client = new() { BaseAddress = new Uri("https://api.open-meteo.com") };
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            
+            Random rnd = new();
+
+            string path = "/v1/forecast?latitude=49.1011&longitude=-122.6588&current=temperature_2m&daily=temperature_2m_max,temperature_2m_min&forecast_days=1";
+
+            try
+            {
+                HttpResponseMessage response = client.GetAsync(path).Result;
+                
+                if (response.IsSuccessStatusCode)
+                {
+                    ret = response.Content.ReadAsStringAsync().Result;
+                }
+                else
+                {
+                    ret = "Calling the service got " + response.StatusCode + "\r\n" + response.Content;
+                }
+            }
+            catch (Exception ex)
+            {
+                ret = "Erro calling " + client.BaseAddress + "\r\n" + ex.ToString();
+            }
+        }
+        catch
+        {
+            ret = "Error calling Weather API";
+        }
 
         return ret;
     }    
